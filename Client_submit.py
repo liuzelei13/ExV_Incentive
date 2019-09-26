f58454fcd14fdf24924e9466d7b32531a18289cd
liuzelei13 <liuzelei@foxmail.com>
2019-09-26 14:03:48 +0800

    test1

---
+++ Client_submit.py
@@ -0,0 +1,20 @@
+import Server_Incentive as SI
+
+
+#each user when participant in new round the federation training, after test its new
+# prepared data with newest model and get the test result as 'data_metric_result',
+# calls the submit_results function.
+
+#Server_Incentive is the function as server end to receive the parameter. User only
+# has to upload the parameters.
+
+#todo but user must upload the new_model_name it is about to participating in.
+# OR if there is no such new_model_name can be provided. then it can be give at
+# 'add_user' function at the server end.
+
+
+def submit_results(user_name,data_quantity,data_metric_result,new_model_name):
+    j={}
+    SI.add(j)
+    SI.add_user(user_name,data_quantity,data_metric_result,new_model_name)
+    pass
---
+++ run_example.py
@@ -0,0 +1,40 @@
+import Server_Incentive as SI
+import Client_submit as CS
+
+
+'''
+this is a rundown for user and server end of the incentive mechanism interaction.
+
+Followings are some things to be considered:
+1.Function CS.submit_results needs parameter 'new_model_name' 
+  when user submit their new data to server.
+  This parameter can either be submitted by user or given by server
+  after user's submission.
+  User submission only pass the parameters to server.
+
+2. The final result is in global dict variable FI.Model at server
+  end.
+  It can be changed to write into local file or insert into
+  database. Just change the last line of function add_model.
+'''
+
+#fisrt, server add the initial model in
+SI.add_model('m0','m0',0.75)
+
+#client start participant and submit their info round 1
+CS.submit_results('u1',100,0.75,'m1')
+CS.submit_results('u2',80,0.6,'m1')
+
+#server finishes training model and add the new model in
+SI.add_model('m0','m1',0.76)
+
+#client start participant and submit their info round 2
+CS.submit_results('u1',100,0.8,'m2')
+CS.submit_results('u3',50,0.8,'m2')
+
+#server finishes training model and add the new model in
+SI.add_model('m1','m2',0.77)
+
+#all models info and corresponding incentive plan/distribution are store
+# in SI.Models
+print(SI.Models)
